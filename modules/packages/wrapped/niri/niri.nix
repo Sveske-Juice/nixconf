bf4bd3f5a5c29a194b0582dcd6336922fc9c85ad
Sveske-Juice <sveske_juice@tuta.com>
2026-04-07 11:16:33 +0200

    feat(pkgs/niri): init

---
+++ modules/packages/wrapped/niri/niri.nix
@@ -0,0 +1,16 @@
+{self, inputs, ...}: {
+  flake.wrappersModules.niri = {lib, pkgs, ...}: {
+      settings = {
+        binds = {
+          "Mod+Return".spawn = "${lib.getExe pkgs.kitty}";
+        };
+      };
+  };
+
+  perSystem = {pkgs, ...}: {
+    packages.niri = inputs.wrapper-modules.wrappers.niri.wrap {
+      inherit pkgs;
+      imports = [self.wrappersModules.niri];
+    };
+  };
+}
