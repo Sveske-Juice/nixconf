5614c1ab2c3d7be762abc33760c6753c44768ad0
Sveske-Juice <sveske_juice@tuta.com>
2026-04-07 12:21:47 +0200

    feat(general): root user sops

---
+++ modules/nixos/optional/general/root-user.nix
@@ -0,0 +1,19 @@
+# Generates root user
+{
+  flake.nixosModules.general = {
+    lib,
+    config,
+    ...
+  }: {
+    users.mutableUsers = false;
+
+    sops.secrets."users/root" = lib.mkIf config.preferences.secrets {
+      neededForUsers = true;
+    };
+
+    users.users.root = {
+      password = lib.mkIf (!config.preferences.secrets) "root";
+      hashedPasswordFile = lib.mkIf config.preferences.secrets config.sops.secrets."users/root".path;
+    };
+  };
+}
