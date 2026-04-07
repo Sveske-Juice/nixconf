6db052eefc6c89aaf115b139a23ecfb6de76616f
Sveske-Juice <sveske_juice@tuta.com>
2026-04-07 17:34:01 +0200

    feat(users/dr3y): public gpg key

--- modules/nixos/users/dr3y/default.nix
+++ modules/nixos/users/dr3y/default.nix
@@ -3,6 +3,7 @@ _: {
     preferences.user = {
       name = "dr3y";
       email = "sveske_juice@tuta.com";
+      gpgKey = "44B31C98B111DF02";
     };
 
     deploy-gpg.enable = config.preferences.secrets;
