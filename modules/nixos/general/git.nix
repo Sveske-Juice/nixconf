3bfa2c28480e214c3ce0905554df849f26373e2b
Sveske-Juice <sveske_juice@tuta.com>
2026-04-07 10:02:01 +0200

    feat(mod/general): git

---
+++ modules/nixos/general/git.nix
@@ -0,0 +1,46 @@
+_: {
+  flake.nixosModules.general = {pkgs, lib, config, ...}: {
+    programs.git = {
+      enable = true;
+      package = pkgs.gitFull;
+
+      config = {
+        user = {
+          inherit (config.preferences.user) name email;
+          signingkey = lib.mkIf (config.preferences.user.gpgKey != null) config.preferences.user.gpgKey;
+        };
+        safe.directory = [ "/etc/nixos" ];
+        commit = {
+          gpgsign = lib.mkIf (config.preferences.user.gpgKey != null) true;
+        };
+        core = {
+          whitespace = "error";
+        };
+        status = {
+          branch = true;
+          short = true;
+          showStash = true;
+        };
+        push = {
+          autoSetupRemote = true;
+          followTags = true;
+        };
+        pull = {
+          rebase = true;
+        };
+        rebase = {
+          autoStash = true;
+        };
+        url = {
+          "https://github.com/" = {
+            insteadOf = [
+              "gh:"
+              "github:"
+            ];
+          };
+        };
+      };
+    };
+  };
+}
+
