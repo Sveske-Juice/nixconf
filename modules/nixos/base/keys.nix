bc1799459c1a780b958656434d8203b82574b91a
Sveske-Juice <sveske_juice@tuta.com>
2026-04-25 15:54:21 +0200

    feat(base): pub key management with options.keys and options.keyGroups

---
+++ modules/nixos/base/keys.nix
@@ -0,0 +1,34 @@
+{
+  flake.nixosModules.base = {lib, config, ...}: {
+    options = {
+      keyGroups = lib.mkOption {
+        default = {};
+        description = "Attrs of named groups of keys";
+        type = lib.types.attrsOf (lib.types.listOf lib.types.str);
+      };
+      keys = lib.mkOption {
+        default = {};
+        type = lib.types.submodule {
+          options = {
+            ssh = lib.mkOption {
+              default = {};
+              description = "Named public ssh keys";
+              type = lib.types.attrsOf lib.types.str;
+              example = {
+                "host1" = "pubkey1";
+                "host2" = "pubkey2";
+              };
+            };
+          };
+        };
+      };
+    };
+    config = {
+      keyGroups.all = builtins.attrValues config.keys.ssh;
+
+      keys.ssh = {
+        "pixel9a" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKeDUjke9DCqWRgQy7UFiCiTeIid6pXyzSTqYWS7MDf9 u0_a254@localhost";
+      };
+    };
+  };
+}
