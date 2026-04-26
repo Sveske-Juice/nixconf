4329de428278d5b61e298f3f2517a2f13764380f
dr3y <sveske_juice@tuta.com>
2026-04-26 12:46:38 +0200

    fix: rebuild alias use --sudo

--- modules/nixos/optional/general/shell/aliases.nix
+++ modules/nixos/optional/general/shell/aliases.nix
@@ -1,9 +1,9 @@
 {
   flake.nixosModules.general = _: {
     programs.fish.shellAbbrs = {
-      "nrs" = "sudo nixos-rebuild switch --flake .#";
-      "nrb" = "sudo nixos-rebuild build --flake .#";
-      "nrv" = "sudo nixos-rebuild build-vm --flake .#";
+      "nrs" = "nixos-rebuild switch --flake .# --sudo";
+      "nrb" = "nixos-rebuild build --flake .# --sudo";
+      "nrv" = "nixos-rebuild build-vm --flake .#";
 
       "gp" = "git push";
       "gu" = "git pull";
