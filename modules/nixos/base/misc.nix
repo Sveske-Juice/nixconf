d3129f91236f6cab6de875b0ad2bf425204e9756
dr3y <sveske_juice@tuta.com>
2026-06-28 22:56:32 +0200

    feat(base): trusted users for remote rebuilds

--- modules/nixos/base/misc.nix
+++ modules/nixos/base/misc.nix
@@ -1,9 +1,12 @@
-_: {
+{
   flake.nixosModules.base = _: {
     nix.settings.experimental-features = [
       "nix-command"
       "flakes"
     ];
+
+    # Allow remote rebuilds
+    nix.settings.trusted-users = [ "root" "@wheel" ];
 
     # Takes forever on rebuild, don't need it
     documentation.man.cache.enable = false;
