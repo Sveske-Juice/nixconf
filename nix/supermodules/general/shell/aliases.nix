e1bc0e41f5ca7cc7cedf1a01f1f90ee953fcedee
dr3y <sveske_juice@tuta.com>
2026-06-30 13:52:04 +0200

    fix(fish): nrrs alias

--- nix/supermodules/general/shell/aliases.nix
+++ nix/supermodules/general/shell/aliases.nix
@@ -2,7 +2,7 @@
   flake.nixosModules.general = _: {
     programs.fish.shellAbbrs = {
       "nrs" = "nixos-rebuild switch --flake .# --sudo";
-      "nrrs" = "nixos-rebuild switch --sudo --ask-sudo-password --flake .#host host@ip";
+      "nrrs" = "nixos-rebuild switch --sudo --ask-sudo-password --flake .#host --target-host host@ip";
       "nrb" = "nixos-rebuild build --flake .# --sudo";
       "nrv" = "nixos-rebuild build-vm --flake .#";
 
