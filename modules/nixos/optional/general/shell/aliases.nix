c92b3dfac60cbd3e0ffd59071b803c221d544621
dr3y <sveske_juice@tuta.com>
2026-06-30 12:53:47 +0200

    feat(fish): remote rebuild alias

--- modules/nixos/optional/general/shell/aliases.nix
+++ modules/nixos/optional/general/shell/aliases.nix
@@ -2,6 +2,7 @@
   flake.nixosModules.general = _: {
     programs.fish.shellAbbrs = {
       "nrs" = "nixos-rebuild switch --flake .# --sudo";
+      "nrrs" = "nixos-rebuild switch --flake --sudo --ask-sudo-password --target-host host@ip .#host";
       "nrb" = "nixos-rebuild build --flake .# --sudo";
       "nrv" = "nixos-rebuild build-vm --flake .#";
 
