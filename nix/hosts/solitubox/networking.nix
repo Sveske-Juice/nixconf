afaa6fc1c9ceef6f42c6f02d8de9afae9437fa46
dr3y <sveske_juice@tuta.com>
2026-07-12 16:41:09 +0200

    feat(solitubox): test port

--- nix/hosts/solitubox/networking.nix
+++ nix/hosts/solitubox/networking.nix
@@ -1,5 +1,8 @@
 {
   flake.nixosModules.host-solitubox = {
     networking.networkmanager.enable = true;
+
+    networking.firewall.allowedTCPPorts = [ 6767 ];
+    networking.firewall.allowedUDPPorts = [ 6767 ];
   };
 }
