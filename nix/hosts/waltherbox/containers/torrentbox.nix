059648a781688e707ff195f9afc32f5c97592a2d
dr3y <sveske_juice@tuta.com>
2026-07-28 22:57:03 +0200

    feat(waltherbox): qbittorrent

--- nix/hosts/waltherbox/containers/torrentbox.nix
+++ nix/hosts/waltherbox/containers/torrentbox.nix
@@ -1,9 +1,16 @@
 {self, ...}: {
   flake.nixosModules.ct-torrentbox = let
     name = "torrentbox";
+    # mullvad dns
     dns = "10.64.0.1";
+    qbitPort = 9091;
+
+    qbitConfigDir = "/var/lib/qbittorrent";
+    qbitDataDir = "/data";
+    hostQbitConfigDir = "/fast/apps/qbittorrent/config";
+    hostQbitDataDir = "/fast/apps/qbittorrent/data";
   in
-    {config, ...}: {
+    {config, pkgs, ...}: {
       sops.secrets = {
         "mullvad/private-key" = {};
         "mullvad/address" = {};
@@ -48,6 +55,12 @@
         mode = "0400";
       };
 
+      containers.proxy.config.services.caddy.virtualHosts."torrent.lan.waltherbox.org".extraConfig = ''
+        import cf_tls
+        import lan_only
+        reverse_proxy http://192.168.1.75:${toString qbitPort}
+      '';
+
       containers."${name}" = {
         autoStart = true;
         privateNetwork = true;
@@ -55,6 +68,7 @@
         localAddress = "192.168.1.75/24";
         extraFlags = ["--resolv-conf=off"];
 
+        # Allow container to access TUN device
         allowedDevices = [
           {
             node = "/dev/net/tun";
@@ -63,9 +77,13 @@
         ];
 
         # Import rendered wg config to container
-        bindMounts."/etc/wireguard/wg0.conf" = {
-          hostPath = config.sops.templates."wg0.conf".path;
-          isReadOnly = true;
+        bindMounts = {
+          "/etc/wireguard/wg0.conf" = {
+            hostPath = config.sops.templates."wg0.conf".path;
+            isReadOnly = true;
+          };
+          ${qbitConfigDir} = { hostPath = hostQbitConfigDir; isReadOnly = false; };
+          ${qbitDataDir} = { hostPath = hostQbitDataDir; isReadOnly = false; };
         };
 
         config = {
@@ -81,6 +99,57 @@
               configFile = "/etc/wireguard/wg0.conf";
               autostart = true;
             };
+          };
+
+          # Create container local group with matching gid of host group
+          users.groups.media.gid = config.users.groups.media.gid;
+
+          services.qbittorrent = {
+            enable = true;
+            webuiPort = qbitPort;
+            torrentingPort = 6881;
+            openFirewall = true;
+
+            profileDir = qbitConfigDir;
+            group = "media";
+
+            serverConfig = {
+              LegalNotice.Accepted = true;
+              BitTorrent.Session = {
+                IPv6Enabled = true;
+                DefaultSavePath = qbitDataDir;
+                TempPathEnabled = false;
+
+                DHTEnabled = true;
+                DHT = true;
+                PeXEnabled = true;
+                AnnounceToAllTrackers = true;
+                AnnounceToAllTiers = true;
+                # Won't do anything because we connect through vpn
+                UseUPnP = false;
+
+                GlobalMaxConnections = 500;
+                GlobalMaxUploads = 50;
+                MaxConnectionsPerTorrent = 100;
+                MaxUploadsPerTorrent = 10;
+              };
+              Preferences = {
+                General.Locale = "en";
+
+                WebUI = {
+                  Address = "*";
+                  Port = qbitPort;
+                  Username = "qbitadmin";
+                  Password_PBKDF2 = "CMTz2RxO4FOFV/5MJfuF6A==:SK0skoW92U/vEzLyX5ovY2uM3KSbv80H8OSN56PhbmUdBHn6/g6QSVQBAPQgonYDRGer2SY5vpXf8x0TYkAuiw==";
+
+                  # AuthSubnetWhitelistEnabled = true;
+                  # AuthSubnetWhitelist = "192.168.1.0/24";
+                  AlternativeUIEnabled = true;
+                  RootFolder = "${pkgs.vuetorrent}/share/vuetorrent";
+                };
+              };
+            };
+            extraArgs = [ "--confirm-legal-notice" ];
           };
 
           preferences.host.name = "${name}";
